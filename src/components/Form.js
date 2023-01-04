eee6b4612ee2b53ba9c4c3799d942e83d8798ae1
bartek dec <decbartek@gmail.com>
2023-01-04 11:49:15 +0100

    added Form component

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {GoChevronRight} from 'react-icons/go';
+
+const Form = () => {
+    return (
+        <section className='section'>
+            <h1 className='section-title'>IP Address Tracker</h1>
+            <form className='form center'>
+                <input type='text' className='form-input'
+                placeholder='Search for any IP address or domain'/>
+                <button type='submit' className='btn'>
+                    <GoChevronRight/>
+                </button>
+            </form>
+        </section>
+    );
+};
+
+export default Form;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Form} from '../components';
+
+const LandingPage = () => {
+    return (
+        <main className='main'>
+            <Form/>
+        </main>
+    );
+};
+
+export default LandingPage;
