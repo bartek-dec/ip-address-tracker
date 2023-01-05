d157d6e3efa83b86a99e6c9873225f522c667bef
bartek dec <decbartek@gmail.com>
2023-01-05 20:14:33 +0100

    created Spinner component

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <section className='loading-container'>
+            <div className='loading'>
+
+            </div>
+        </section>
+    );
+};
+
+export default Spinner;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Form from "./Form";
 import MapComp from './MapComp';
 import Card from "./Card";
+import Spinner from "./Spinner";
 
-export {Form, MapComp, Card}
+export {Form, MapComp, Card, Spinner}
